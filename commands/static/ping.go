2d4bf28839b38fdb15741e66837da02abbdbeb0f
Starshine System <us@starshines.xyz>
2021-03-11 18:56:22 +0100

    woops

--- commands/static/ping.go
+++ commands/static/ping.go
@@ -8,18 +8,19 @@ import (
 )
 
 func (bot *Bot) ping(ctx *bcr.Context) (err error) {
+	t := time.Now()
 	// this will return 0ms in the first minute after the bot is restarted
 	// can't do much about that though
 	heartbeat := ctx.Session.Gateway.PacerLoop.EchoBeat.Time().Sub(ctx.Session.Gateway.PacerLoop.SentBeat.Time()).Round(time.Millisecond)
 
-	t := time.Now()
-	m, err := ctx.Send("Pong!", nil)
+	s := fmt.Sprintf("🏓 **Pong!**\nHeartbeat: %v", heartbeat)
+	m, err := ctx.Send(s, nil)
 	if err != nil {
 		return err
 	}
 
-	_, err = ctx.Edit(m, fmt.Sprintf(
-		"Ping: %v | Message: %v", heartbeat, time.Since(t).Round(time.Millisecond),
-	), nil)
+	latency := time.Since(t).Round(time.Millisecond)
+
+	_, err = ctx.Edit(m, fmt.Sprintf("%v\nMessage: %v", s, latency), nil)
 	return err
 }
