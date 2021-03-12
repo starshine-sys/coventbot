678a662cc8c4317e5746edf6d4614130fc3c2018
Starshine System <us@starshines.xyz>
2021-03-12 11:54:36 +0100

    tweak ping command

--- commands/static/info/ping.go
+++ commands/static/info/ping.go
@@ -9,18 +9,18 @@ import (
 
 func (bot *Bot) ping(ctx *bcr.Context) (err error) {
 	t := time.Now()
-	// this will return 0ms in the first minute after the bot is restarted
-	// can't do much about that though
-	heartbeat := ctx.Session.Gateway.PacerLoop.EchoBeat.Time().Sub(ctx.Session.Gateway.PacerLoop.SentBeat.Time()).Round(time.Millisecond)
 
-	s := fmt.Sprintf("🏓 **Pong!**\nHeartbeat: %v", heartbeat)
-	m, err := ctx.Send(s, nil)
+	m, err := ctx.Send("Pong!", nil)
 	if err != nil {
 		return err
 	}
 
 	latency := time.Since(t).Round(time.Millisecond)
 
-	_, err = ctx.Edit(m, fmt.Sprintf("%v\nMessage: %v", s, latency), nil)
+	// this will return 0ms in the first minute after the bot is restarted
+	// can't do much about that though
+	heartbeat := ctx.Session.Gateway.PacerLoop.EchoBeat.Time().Sub(ctx.Session.Gateway.PacerLoop.SentBeat.Time()).Round(time.Millisecond)
+
+	_, err = ctx.Edit(m, fmt.Sprintf("Heartbeat: %v | Message: %v", heartbeat, latency), nil)
 	return err
 }
