3a069359cfa90d48ed1094dcb9e34be507b40fdf
Starshine System <us@starshines.xyz>
2021-06-22 22:32:31 +0200

    make tags ping the replied to message

--- bot/message_create.go
+++ bot/message_create.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/diamondburned/arikawa/v2/api"
 	"github.com/diamondburned/arikawa/v2/api/webhook"
 	"github.com/diamondburned/arikawa/v2/discord"
 	"github.com/diamondburned/arikawa/v2/gateway"
+	"github.com/diamondburned/arikawa/v2/utils/json/option"
 	"github.com/starshine-sys/bcr"
 )
 
@@ -120,11 +122,26 @@ func (bot *Bot) handleTagCommand(ctx *bcr.Context) (err error) {
 		return nil
 	}
 
-	m := ctx.NewMessage().Content(t.Response).BlockMentions()
-	// reply to the same message the invocation replied to
-	if ctx.Message.Reference != nil {
-		m.Reference(ctx.Message.Reference.MessageID)
+	tr := false
+
+	if len(ctx.Message.Mentions) > 0 {
+		tr = true
 	}
-	_, err = m.Send()
+
+	data := api.SendMessageData{
+		Content: t.Response,
+		AllowedMentions: &api.AllowedMentions{
+			Parse:       []api.AllowedMentionType{},
+			RepliedUser: option.Bool(&tr),
+		},
+	}
+
+	if ctx.Message.Reference != nil {
+		data.Reference = &discord.MessageReference{
+			MessageID: ctx.Message.Reference.MessageID,
+		}
+	}
+
+	_, err = ctx.State.SendMessageComplex(ctx.Message.ChannelID, data)
 	return err
 }
--- commands/tags/tag.go
+++ commands/tags/tag.go
@@ -1,6 +1,9 @@
 package tags
 
 import (
+	"github.com/diamondburned/arikawa/v2/api"
+	"github.com/diamondburned/arikawa/v2/discord"
+	"github.com/diamondburned/arikawa/v2/utils/json/option"
 	"github.com/starshine-sys/bcr"
 )
 
@@ -11,11 +14,26 @@ func (bot *Bot) tag(ctx *bcr.Context) (err error) {
 		return
 	}
 
-	m := ctx.NewMessage().Content(t.Response).BlockMentions()
-	// reply to the same message the invocation replied to
-	if ctx.Message.Reference != nil {
-		m.Reference(ctx.Message.Reference.MessageID)
+	tr := false
+
+	if len(ctx.Message.Mentions) > 0 {
+		tr = true
 	}
-	_, err = m.Send()
+
+	data := api.SendMessageData{
+		Content: t.Response,
+		AllowedMentions: &api.AllowedMentions{
+			Parse:       []api.AllowedMentionType{},
+			RepliedUser: option.Bool(&tr),
+		},
+	}
+
+	if ctx.Message.Reference != nil {
+		data.Reference = &discord.MessageReference{
+			MessageID: ctx.Message.Reference.MessageID,
+		}
+	}
+
+	_, err = ctx.State.SendMessageComplex(ctx.Message.ChannelID, data)
 	return err
 }
