0e997d4f97ed996a6c44038b9a2aeca20554aa74
Sam <us@starshines.xyz>
2022-04-12 01:46:32 +0200

    feat: default permission nodes/levels

--- common/permissions_default.go
+++ common/permissions_default.go
@@ -1,9 +1,110 @@
 package common
 
 var DefaultPermissions = Nodes{
+	// level commands
 	{"level.*", EveryoneLevel},
+	{"level.setxp", ManagerLevel},
+	{"level.setlvl", ManagerLevel},
 	{"level.config.*", ManagerLevel},
+	{"level.background.server", ManagerLevel},
+	{"nolevels.*", ModeratorLevel},
+	{"leaderboard", EveryoneLevel},
+
+	// tickets
+	{"tickets.*", EveryoneLevel},
+	{"tickets.config.*", ManagerLevel},
+	{"tickets.list", ManagerLevel},
+
+	// roles
+	{"roles", EveryoneLevel},
+	{"roles.config.*", ManagerLevel},
+	{"role", EveryoneLevel},
+	{"role.info", EveryoneLevel},
+	{"role.dump", ModeratorLevel},
+	{"role.create", ManagerLevel},
+	{"derole", EveryoneLevel},
+
+	// quotes
+	{"quote.*", EveryoneLevel},
+	{"quote.delete", ModeratorLevel},
+	{"quotes", EveryoneLevel},
+	{"quotes.leaderboard", EveryoneLevel},
+	{"quotes.toggle", ManagerLevel},
+	{"quotes.messages", ManagerLevel},
+
+	// self-mute/pause
+	{"muteme", EveryoneLevel},
+	{"pauseme", EveryoneLevel},
+	{"muteme.message", ManagerLevel},
+
+	// moderation
+	{"warn", ModeratorLevel},
+	{"unban", ManagerLevel},
+	{"setnote", ModeratorLevel},
+	{"reason", ModeratorLevel},
+	{"purge", ModeratorLevel},
+	{"notes", ModeratorLevel},
+	{"modlog", ModeratorLevel},
+	{"members", ModeratorLevel},
+	{"massban", ManagerLevel},
+	{"makeinvite", ManagerLevel},
+	{"lockdown", ManagerLevel},
+	{"exportemotes", ManagerLevel},
+	{"embed.*", ManagerLevel},
+	{"echo.*", ManagerLevel},
+	{"delnote", ModeratorLevel},
+	{"bgc", ModeratorLevel},
+	{"ban", ManagerLevel},
+	{"approve", ManagerLevel},
+	{"addemoji", ManagerLevel},
+
+	// configuration
+	{"muterole", ManagerLevel},
+	{"pauserole", ManagerLevel},
+	{"reactroles.*", ManagerLevel},
+	{"prefix.*", ManagerLevel},
 	{"permissions.*", AdminLevel},
+	{"watchlist.*", ManagerLevel},
+	{"triggers.*", ManagerLevel},
+	{"starboard.*", ManagerLevel},
+	{"slowmode.*", ManagerLevel},
+	{"modlog.import", ManagerLevel},
+	{"modlog.export", ManagerLevel},
+	{"modlog.channel", ManagerLevel},
+	{"keyrole.*", ManagerLevel},
+	{"cc.*", ManagerLevel},
+	{"approval.*", ManagerLevel},
+
+	// user commands
+	{"todo.*", EveryoneLevel},
+	{"remindme.*", EveryoneLevel},
+	{"userinfo", EveryoneLevel},
+	{"user-cfg", EveryoneLevel},
+	{"transcript", ManagerLevel},
+	{"serverinfo", EveryoneLevel},
+	{"sampa", EveryoneLevel},
+	{"roll", EveryoneLevel},
+	{"roleinfo", EveryoneLevel},
+	{"reminders", EveryoneLevel},
+	{"quickpoll", EveryoneLevel},
+	{"pride", EveryoneLevel},
+	{"poll", EveryoneLevel},
+	{"ping", EveryoneLevel},
+	{"message", EveryoneLevel},
+	{"meow", EveryoneLevel},
+	{"linkto", EveryoneLevel},
+	{"invite", EveryoneLevel},
+	{"idtime", EveryoneLevel},
+	{"help", EveryoneLevel},
+	{"getinvite", EveryoneLevel},
+	{"enlarge", EveryoneLevel},
+	{"embedsource", EveryoneLevel},
+	{"delreminder", EveryoneLevel},
+	{"complete", EveryoneLevel},
+	{"colour", EveryoneLevel},
+	{"bubble", EveryoneLevel},
+	{"avatar", EveryoneLevel},
+	{"about", EveryoneLevel},
 }
 
 var defaultPermsAreSorted = false
